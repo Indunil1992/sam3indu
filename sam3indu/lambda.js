740e0796bd6b02ce71f1ffee28b74a422508225a
Indunil1992 <indunil@adroitlogic.com>
2019-12-16 13:25:38 +0530

    2

--- sam3indu/lambda.js
+++ sam3indu/lambda.js
@@ -2,17 +2,18 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
+    debugger;
     ddb.get({
         TableName: 'testIndu',
         Key: { 'test': '10' }
     }).promise()
         .then((data) => {
-                        console.log("dataaaaa");
+            console.log("dataaaaa");
 
             //your logic goes here
         })
         .catch((err) => {
-                        console.log("errrrrrr");
+            console.log("errrrrrr");
 
             //handle error
         });
